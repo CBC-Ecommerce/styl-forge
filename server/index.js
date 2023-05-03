e8b860d0fc984136b215e9c323e2bcf18fcccbe8
JackMartinuzzi <jjmartinuzzi@gmail.com>
2023-05-03 15:53:01 -0600

    Added AirBnB linter and set up server/index.js to run with server-dev

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 // server to query API
 require('dotenv').config();
-const express = require("express");
-const axios = require("axios");
+const express = require('express');
+const axios = require('axios');
 
-// listne on port from .env
+const app = express();
+
+// listen on port from .env
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 // require path for join, require HTML plugin
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   // define entry point
-  entry: path.join(__dirname, "/client/src/index.js"),
+  entry: path.join(__dirname, '/client/src/index.js'),
   // define output location
-  output: path.join(__dirname, "/client/dist"),
+  output: path.join(__dirname, '/client/dist'),
   // define rules for webpack to use with babel
   module: {
     rules: [
@@ -15,19 +16,19 @@ module.exports = {
         // exclude node_modules from transpile
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           // transpile to browser recognizable versions of code
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
-    ]
+    ],
   },
   // use the following plugins
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
+      template: path.join(__dirname, 'src', 'index.html'),
     }),
   ],
-}
+};
