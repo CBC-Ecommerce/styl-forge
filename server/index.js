92d8a714f9483473daedf9081469c1307d762682
JackMartinuzzi <jjmartinuzzi@gmail.com>
2023-05-03 15:34:07 -0600

    Added webpack and babel dev-dependencies and webpack.config.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
 // server to query API
+require('dotenv').config();
+const express = require("express");
+const axios = require("axios");
+
+// listne on port from .env
+app.listen(process.env.PORT);
+console.log(`Listening at http://localhost:${process.env.PORT}`);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// require path for join, require HTML plugin
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  // define entry point
+  entry: path.join(__dirname, "/client/src/index.js"),
+  // define output location
+  output: path.join(__dirname, "/client/dist"),
+  // define rules for webpack to use with babel
+  module: {
+    rules: [
+      {
+        // transpile files that end in '.js'
+        test: /\.?js$/,
+        // exclude node_modules from transpile
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          // transpile to browser recognizable versions of code
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  },
+  // use the following plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+}
