e7ab2d65c3fde599aa49597356cade1db10b6243
Rachel <rkmiller131@gmail.com>
2023-05-04 16:28:20 -0700

    Functioning Get Request

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ const path = require('path');
 const routes = require('./routes');
 
 const app = express();
-
-app.get('/products', routes.getProductByID);
-
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/products', routes.getProductByID);
+
 
 // listen on port from .env
 const port = 3000;
--- server/routes.js
+++ server/routes.js
@@ -1,23 +1,25 @@
 const axios = require('axios');
 
 const authHeader = {
-  Authorization: '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',
+  headers: {
+    Authorization: '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',
+  },
 };
-const apiURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/:hr-rfp/';
+const apiURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/';
 
 // '/product?id=5' {id: 5}
 
 
 exports.getProductByID = function (req, res) {
-  const url = `${apiURL}/products:${req.query.productId}`;
-  console.log(req.query, 'this is req.query*******');
+  const url = `${apiURL}products/${req.query.product_id}`;
+  console.log(req.query.product_id, 'this is req.query*******');
 
 
-  axios.get(url, {headers: authHeader})
+  axios.get(url, authHeader)
 
   // axios({ url: apiURL, method: 'get', headers: authHeader, params: req.query})
     .then((results) => {
-      res.status(200).json(results);
+      res.status(200).send(results.data);
     })
     .catch((err) => {
       console.log('There was an error: ', err);
