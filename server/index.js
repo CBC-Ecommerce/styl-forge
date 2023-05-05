36dd1f6b514eadfc54b318b0683ec3185f6a3dd5
yluo12 <yantingluo98@gmail.com>
2023-05-05 16:28:02 -0700

    finished QA routes

--- server/index.js
+++ server/index.js
@@ -24,6 +24,15 @@ app.post('/reviews', controllers.addReviews);
 app.put('/reviews/helpful', controllers.markReviewHelpful);
 app.put('/reviews/report', controllers.reportReview);
 
+app.get('/qa/questions', controllers.getQuestions);
+app.get('/qa/questions/answers', controllers.getAnswers);
+app.post('/qa/questions', controllers.addAQuestion);
+app.post('/qa/questions/answers', controllers.addAnswer);
+app.put('/qa/questions/helpful', controllers.markQuestionHelpful);
+app.put('/qa/questions/report', controllers.reportQuestion);
+app.put('/qa/answers/helpful', controllers.markAnswerHelpful);
+app.put('/qa/answers/report', controllers.reportAnswer);
+
 // Establish connection to port
 const port = process.env.PORT;
 app.listen(port);
