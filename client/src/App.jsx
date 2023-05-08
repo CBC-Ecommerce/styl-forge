6157b3f1746d2a7014bfa293424a890e6f86c427
aitzeng <aitzeng@gmail.com>
2023-05-08 09:12:10 -0700

    Updated App to include QnA

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import RelatedProducts from './RelatedItems/RelatedProducts.jsx';
+import QnA from './QnA/QnA.jsx';
 
 function App() {
   const [id, setId] = useState(1);
   return (
     <div>
       Hello world!
+      <QnA id={id} />
       <RelatedProducts id={id} />
     </div>
   );
