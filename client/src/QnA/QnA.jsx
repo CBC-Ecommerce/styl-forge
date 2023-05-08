c059a7d04543e7e66ed97e311e3beca1cef83d1a
aitzeng <aitzeng@gmail.com>
2023-05-08 12:23:00 -0700

    Added axios request to grab questions

--- client/src/QnA/QnA.jsx
+++ client/src/QnA/QnA.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import QnAList from './QnAList.jsx';
 
 const { useState, useEffect } = React;
 
 function QnA({ id }) {
-  // useEffect(() => {
-  //   axios('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/')
-  // }, []);
   const [quests, setQuests] = useState([]);
+
+  const grabQuestions = () => {
+    axios(`/qa/questions?product_id=${id}&page=1&count=9999`)
+      .then((response) => {
+        setQuests(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    grabQuestions();
+  }, []);
 
   return (
 
