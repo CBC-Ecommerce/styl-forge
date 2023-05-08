7604479508a8b0fb0c78efb05e83b566adbad1eb
aitzeng <aitzeng@gmail.com>
2023-05-08 14:05:06 -0700

    Updated to add answer map

--- client/src/QnA/QnAListEntry.jsx
+++ client/src/QnA/QnAListEntry.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import AnswerListEntry from './AnswerListEntry.jsx';
 
 function QnAListEntry({ quest }) {
+  const [answers, setAnswers] = useState([]);
+  const grabAnswers = () => {
+    axios(`/qa/questions/${quest.question_id}/answers?page=1&count=9999`)
+      .then((info) => {
+        setAnswers(info.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    grabAnswers();
+  }, []);
   return (
 
     <div>
-      Question:
-      {' '}
-      {quest.question_body}
+      <div>
+        Q:
+        {' '}
+        {quest.question_body}
+      </div>
+      <div>
+        A:
+        {' '}
+        {answers.slice(0, 2)
+          .map((answer) => <AnswerListEntry answer={answer} key={answer.answer_id} />)}
+      </div>
     </div>
   );
 }
