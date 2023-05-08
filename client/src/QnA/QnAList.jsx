c1f8a1423c0192d81fc36fb4bae2cf9f22e2a4cf
aitzeng <aitzeng@gmail.com>
2023-05-08 12:23:42 -0700

    Updated to render two at a time, display more questions button

--- client/src/QnA/QnAList.jsx
+++ client/src/QnA/QnAList.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import QnAListEntry from './QnAListEntry.jsx';
 
-function QnAList({quests}) {
+function QnAList({ quests }) {
+  const [numEntry, setNumEntry] = useState(2);
+  const [questButton, setQuestButton] = useState(false);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setNumEntry(numEntry + 2);
+    if (numEntry >= quests.length - 1) {
+      setQuestButton(true);
+    }
+  };
   return (
     <div>
-      {quests.map((quest) => <QnAListEntry quest={quest} key={quest.question_id}/>)}
+      {questButton ? null : (
+        <form onSubmit={submitHandler}>
+          <input type="submit" value="Show More Answered Questions" />
+        </form>
+      )}
+      {quests.slice(0, numEntry)
+        .map((quest) => <QnAListEntry quest={quest} key={quest.question_id} />)}
     </div>
 
   );
