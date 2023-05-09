c6093730c44c33ef2ed2b60f43907e8e8b95d41c
aitzeng <aitzeng@gmail.com>
2023-05-08 20:49:23 -0700

    Updated AnswerEntry to render

--- client/src/QnA/AnswerListEntry.jsx
+++ client/src/QnA/AnswerListEntry.jsx
@@ -2,7 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 function AnswerListEntry({ answer }) {
   return (
-    <div>{answer.body}</div>
+    <div>
+      <div>{answer.body}</div>
+      <div>
+        Helpful?
+        {' '}
+        <button type="submit">
+          Yes
+          {' '}
+          (
+          {answer.helpfulness}
+          )
+        </button>
+      </div>
+    </div>
   );
 }
 
