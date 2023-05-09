28b2ee000403cebf4fc8e36bd4168e41a598821d
aitzeng <aitzeng@gmail.com>
2023-05-08 22:50:48 -0700

    Updated to show Helpful button and Add Answer

--- client/src/QnA/AnswerListEntry.jsx
+++ client/src/QnA/AnswerListEntry.jsx
@@ -1,20 +1,41 @@
 import React, { useState, useEffect } from 'react';
 
 function AnswerListEntry({ answer }) {
+  const date = new Date(answer.date);
+
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+
+  };
+
+  const formattedDate = date.toLocaleDateString('en-US', options);
+
   return (
     <div>
       <div>{answer.body}</div>
-      <div>
+      <span>
+        by
+        {' '}
+        {answer.answerer_name}
+        {' '}
+        {formattedDate}
+        {' '}
         Helpful?
         {' '}
-        <button type="submit">
+        <button type="button">
           Yes
           {' '}
           (
           {answer.helpfulness}
           )
         </button>
-      </div>
+        <button type="button">
+          {' '}
+          Report
+        </button>
+      </span>
     </div>
   );
 }
