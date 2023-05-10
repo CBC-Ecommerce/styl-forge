99d131d75d409273fdc64fd44043520e73343a64
aitzeng <aitzeng@gmail.com>
2023-05-10 13:19:26 -0700

    Updated modal to have input

--- client/src/QnA/AddAnswer.jsx
+++ client/src/QnA/AddAnswer.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function AddAnswer({}) {
+function AddAnswer({ showAdd, addAnswerClicker, quest }) {
+  const [answer, setAnswer] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  if (!showAdd) {
+    return null;
+  }
+  const closeClicker = () => {
+    addAnswerClicker();
+  };
+  const answerChanger = (e) => {
+    setAnswer(e.target.value);
+  };
+  const usernameChanger = (e) => {
+    setUsername(e.target.value);
+  };
+  const emailChanger = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h4 className="modal-title">Modal title</h4>
+    <div className="qna-modal">
+      <div className="qna-modal-content">
+        <div className="qna-modal-header">
+          <h4 className="qna-modal-title">Submit Your Answer</h4>
+          <h5 className="qna-modal-subtitle">Product Name: Question Body</h5>
         </div>
-        <div className="modal-body">
-          This is modal content
+        <div className="qna-modal-body">
+          <form>
+            <input type="text" onChange={answerChanger} placeholder="Answer..." value={answer} />
+            <input type="text" onChange={usernameChanger} placeholder="Username" value={username} />
+            <input type="text" onChange={emailChanger} placeholder="email" value={email} />
+          </form>
         </div>
-        <div className="modal-footer">
-          <button type="button" className="button">Close</button>
+        <div className="qna-modal-footer">
+          <button type="submit">Submit Answer</button>
+          <button type="button" className="button" onClick={closeClicker}>Close</button>
         </div>
       </div>
     </div>
