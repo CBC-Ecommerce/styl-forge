5cf44d37eb58b7db3ea797259090402c67eb9b59
aitzeng <aitzeng@gmail.com>
2023-05-15 19:17:22 -0700

    Added console logs

--- client/src/QnA/QnA.jsx
+++ client/src/QnA/QnA.jsx
@@ -12,6 +12,7 @@ function QnA({ id, product }) {
   const [questModal, setQuestModal] = useState(false);
 
   const grabQuestions = () => {
+    console.log('grabQuestions is invoked');
     const config = { params: { product_id: id, page: 1, count: 99999 } };
     axios.get('/qa/questions', config)
       .then((response) => {
