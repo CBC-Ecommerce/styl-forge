0a8769878655a036ac2cafd2de74d4c0f4dfffee
JackMartinuzzi <jjmartinuzzi@gmail.com>
2023-05-18 17:55:50 -0600

    removed console logs

--- client/src/Overview/GetMousePosition.jsx
+++ client/src/Overview/GetMousePosition.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function GetMousePosition({ img }) {
-  console.log(img)
   // wait for page to load
   window.addEventListener("mousemove", (e) => {
     // set background image
@@ -12,7 +11,6 @@ export default function GetMousePosition({ img }) {
     const y = e.pageY;
     const pageWidth = e.view.innerWidth;
     const pageHeight = e.view.innerHeight;
-    console.log(x, y);
     // set image coordinates
     document.querySelector('#zoomed-image-container').style.setProperty('--x', (x / pageWidth) * 100 + "%");
     document.querySelector('#zoomed-image-container').style.setProperty('--y', (y / pageHeight) * 100 + "%");
